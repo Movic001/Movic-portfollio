efe7171a8cb7c652bf0fd027c0c9678308f037cc
Etzonyx <ibrahimabdulkerim001@gmail.com>
2025-04-09 14:48:20 -0800

    new Features;

---
+++ js/custumGreet.js
@@ -0,0 +1,20 @@
+// write a function that check the clock and greet the user;
+let Greet = document.getElementById("displayGreet");
+let welcome = "Hello";
+
+function greet() {
+  const date = new Date();
+  let Hours = date.getHours();
+  //   let minutes = date.getMinutes();
+  //   let seconds = date.getSeconds();
+  //   console.log("Hours: " + Hours, "minutes: " + minutes, "seconds: " + seconds);
+
+  if (Hours >= 12 && Hours < 12) {
+    Greet.innerHTML = `${welcome}, Good Morning`;
+  } else if (Hours >= 12 && Hours < 16) {
+    Greet.innerHTML = `${welcome}, Good Afternoon `;
+  } else if (Hours >= 16 && Hours < 24) {
+    Greet.innerHTML = `${welcome}, Good Evening`;
+  }
+}
+greet();
